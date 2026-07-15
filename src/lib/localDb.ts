2aba7fff1afd6b2500083d504f9aaba3c388e49d
Diego Rodrigues de Sa e Souza <diegosouza.pw@gmail.com>
2026-07-14 21:20:53 -0300

    chore(db): re-export compressionContextBudget from localDb.ts per db-rules gate (#7005)

--- src/lib/localDb.ts
+++ src/lib/localDb.ts
@@ -93,6 +93,7 @@ export {
 
 export * from "./db/compressionCacheStats";
 export * from "./db/compressionCombos";
+export * from "./db/compressionContextBudget";
 export * from "./db/compressionRunTelemetry";
 export * from "./db/modelContextOverrides";
 
